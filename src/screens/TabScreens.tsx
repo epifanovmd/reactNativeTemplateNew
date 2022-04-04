45ed2b5a33b96901b272120bd59af702696b9299
epifanov <epifanovmd@gmail.com>
2022-04-04 22:26:42 +0300

    ios config and boot splash

--- src/screens/TabScreens.tsx
+++ src/screens/TabScreens.tsx
@@ -1,6 +1,5 @@
 import React, { FC, memo } from "react";
 import { AppNavigation, AppTabScreens, StackProps } from "../navigation";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { Screen1 } from "./screen1";
 import { Screen2 } from "./screen2";
 
@@ -10,25 +9,13 @@ const TAB_SCREENS: AppTabScreens = {
   Page1: {
     screen: Screen1,
     options: {
-      tabBarIcon: ({ color, size }) => (
-        <MaterialCommunityIcons
-          name={"video-wireless"}
-          color={color}
-          size={size}
-        />
-      ),
+      tabBarIcon: ({ color, size }) => null,
     },
   },
   Page2: {
     screen: Screen2,
     options: {
-      tabBarIcon: ({ color, size }) => (
-        <MaterialCommunityIcons
-          name={"video-wireless"}
-          color={color}
-          size={size}
-        />
-      ),
+      tabBarIcon: ({ color, size }) => null,
     },
   },
 };
